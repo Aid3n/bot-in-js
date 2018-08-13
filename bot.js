0a64402673490e049ed08b3576bf7e1589240d1c
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-13 21:05:34 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,59 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const client = new Discord.Client({disableEveryone: true})
+
+//ready -> bot is online!
+client.on("ready", async () => {
+    console.log(`${client.user.username} is online!`);
+    client.user.setActivity("Offline");
+});
+
+//!hello -> Hello!
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("Hello!");
+    }
+})
+
+//what is my avatar -> avatar url
+client.on('message', message => {
+    if (message.content === 'what is my avatar') {
+        return message.channel.send(message.author.avatarURL);
+    }
+});
+
+//!join -> join voice channel
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    
+    if (!message.guild) return;
+    
+    if (message.content === '!join') {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+            .then(connection => {
+                return message.channel.send('Me he unido a tu canal de voz correctamente.');
+            })
+            .catch(console.log);
+        } else {
+            return message.channel.send('Debes estar en un canal de voz primero.');
+        }
+    }
+});
+
+client.login(botconfig.token);
